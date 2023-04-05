8ea83ba912f0905d490fad8b138fe0cece06bca6
Luiz Karpinski <lkastabackup@gmail.com>
2023-04-05 20:48:33 -0300

    NavBar part2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,26 @@
-import {useCallback, useState} from 'react';
-import {BsChevronDown} from 'react-icons/bs';
+import { useCallback, useState } from "react";
+import { BsChevronDown, BsSearch, BsBell } from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
 
 import MobileMenu from "./MobileMenu";
 import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
-const [ShowMobileMenu, setShowMobileMenu] = useState(false);
+  const [ShowMobileMenu, setShowMobileMenu] = useState(false);
+  const [ShowAccountMenu, setShowAccountMenu] = useState(false);
 
-const toggleMobileMenu = useCallback(() => {
-  setShowMobileMenu((current) => !current);
-}, [])
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
 
   return (
     <nav className="w-full fixed z-40">
-      <div className="
+      <div
+        className="
         px-4
         md:px-16
         py-6
@@ -24,31 +31,52 @@ const toggleMobileMenu = useCallback(() => {
         duration-500
         bg-zinc-900
         bg-opciity-90
-      ">
+      "
+      >
         <img className="h-4 lg:h-7" src="/images/logo.png" alt="Logo" />
-        <div className="
+        <div
+          className="
           flex-row
           ml-8
           gap-7
           hidden
           lg:flex
-        ">
-          <NavbarItem label="Home"/>
-          <NavbarItem label="Series"/>
-          <NavbarItem label="Filmes"/>
-          <NavbarItem label="Popular"/>
-          <NavbarItem label="Milnha Lista"/>
-          <NavbarItem label="Browse by Languages"/>
+        "
+        >
+          <NavbarItem label="Home" />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Filmes" />
+          <NavbarItem label="Popular" />
+          <NavbarItem label="Milnha Lista" />
+          <NavbarItem label="Browse by Languages" />
         </div>
-        <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
           <p className="text-white text-sm">Esxplorar</p>
           <BsChevronDown className="text-white transition" />
-          <MobileMenu visible={ShowMobileMenu}/>
+          <MobileMenu visible={ShowMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsBell />
+          </div>
+
+          <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="Profile" />
+            </div>
+            <BsChevronDown className={`text-white transition ${ShowAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
+            <AccountMenu visible={ShowAccountMenu}/>
+          </div>
         </div>
       </div>
-     
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
